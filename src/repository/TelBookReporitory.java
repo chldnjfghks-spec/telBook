7d7802888f3cfa51f999e101837a39fd41dbca01
chldnjfghks-spec <chldnjfghks@gmail.com>
2026-04-22 11:18:46 +0900

    feat : 단일 ID 검색 처리기능

--- src/repository/TelBookReporitory.java
+++ src/repository/TelBookReporitory.java
@@ -56,11 +56,39 @@ public class TelBookReporitory {
                 //System.out.println(dto);
                 //만들어진 dto를 리스트에 담는다
                 dtoList.add(dto);
+
             }
+            psmt.close();
+            rs.close();
         }catch (Exception e){
             System.out.println("Find All Error :"+e.getMessage());
         }
+        //psmt 닫아주는 작업
+
         return dtoList;
     }
 
+    public List<TelDto> findById(int id) {
+        List<TelDto> dtoList = new ArrayList<>();
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
+        try {
+            String sql = "SELET * FROM telbood WHERE id = ?";
+            psmt = conn.prepareStatement(sql);
+            psmt.setLong(1,id);
+            rs = psmt.executeQuery();
+            while(rs.next()){
+                TelDto dto = new TelDto();
+                dto.setId(rs.getLong("id"));
+                dto.setName(rs.getString("name"));
+                dto.setAge(rs.getInt("age"));
+                dto.setAddress(rs.getString("address"));
+                dto.setTelNumber(rs.getString("phone"));
+                dtoList.add(dto);
+            }
+        }catch (Exception e){
+            System.out.println("FindById Error:"+e.getMessage());
+        }
+        return dtoList;
+    }
 }
